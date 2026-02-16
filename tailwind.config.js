7669bc6d672655a1ce33687d8bd68a55a42fe462
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2026-02-16 09:06:23 +0000

    Integrate new fonts and update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                inter: ['Inter', 'sans-serif'],
-                mono: ['Space Mono', 'monospace'],
+                sans: ['Inter', 'sans-serif'],
+                mono: ['JetBrains Mono', 'monospace'],
             },
         },
     },
